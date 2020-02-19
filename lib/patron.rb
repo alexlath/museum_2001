ee8b4fea3ea052c804d2024c992f767cefcd8ac1
Alex Latham <alexlthm@gmail.com>
2020-02-19 09:12:34 -0700

    Add interests to attributes and attr_reader

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,8 +1,9 @@
 class Patron
-  attr_reader :name, :spending_money
+  attr_reader :name, :spending_money, :interests
 
   def initialize(name_param, spending_money_param)
     @name = name_param
     @spending_money = spending_money_param
+    @interests = []
   end
 end
