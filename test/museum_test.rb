b089a6147135929ed7d2a8c198bef3d2c668e3b3
Alex Latham <alexlthm@gmail.com>
2020-02-19 09:16:50 -0700

    Add museum test setup and existence test

---
+++ test/museum_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_can_exist
+    assert_instance_of Museum, @dmns
+  end
