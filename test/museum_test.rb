443c6b5ff35a83a573dd18bfb280f042623da1d6
Alex Latham <alexlthm@gmail.com>
2020-02-19 09:35:48 -0700

    Add test for admitting patrons (with additional patron_3 setup)

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -16,6 +16,8 @@ class MuseumTest < Minitest::Test
     @patron_1.add_interest("Gems and Minerals")
     @patron_2 = Patron.new("Sally", 20)
     @patron_2.add_interest("IMAX")
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
   end
 
   def test_it_can_exist
@@ -43,6 +45,14 @@ class MuseumTest < Minitest::Test
 
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
+
+  def test_it_can_admit_patrons
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
   end
 
 end
